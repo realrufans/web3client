7244f999119766ca3b1d04a2fb5533e0cf5ebd98
solomon stephen <33923021+realrufans@users.noreply.github.com>
2022-07-22 16:37:51 +0100

    env fex

--- client/next.config.js
+++ client/next.config.js
@@ -1,8 +1,11 @@
-const webpack = require('webpack')
-const { parsed: myEnv } = require('dotenv').config()
+const webpack = require("webpack");
+const { parsed: myEnv } = require("dotenv").config({
+  NODE_ENV: "development", // use 'development' unless process.env.NODE_ENV is defined
+  DEBUG: false,
+});
 module.exports = {
-    webpack(config) {
-        config.plugins.push(new webpack.EnvironmentPlugin(myEnv))
-        return config
-    }
-}
+  webpack(config) {
+    config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
+    return config;
+  },
+};
