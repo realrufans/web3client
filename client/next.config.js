60076a09b08dfb0bcc6331de0ef2fa9bf2a646bc
solomon stephen <33923021+realrufans@users.noreply.github.com>
2022-07-22 17:04:17 +0100

    fixed nextconfig

--- client/next.config.js
+++ client/next.config.js
@@ -1,5 +1,5 @@
 const webpack = require('webpack')
-const { parsed: myEnv } = require('dotenv').config()
+const { parsed: myEnv } = require('dotenv')
 module.exports = {
     webpack(config) {
         config.plugins.push(new webpack.EnvironmentPlugin(myEnv))
