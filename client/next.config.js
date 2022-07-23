feab1373006d03d3e06215f108dcbc150f7504a7
solomon stephen <33923021+realrufans@users.noreply.github.com>
2022-07-23 01:29:17 +0100

    removed nextjsconfig

--- client/next.config.js
+++ client/next.config.js
@@ -1,11 +1,1 @@
-require("dotenv").config();
-const webpack = require("webpack");
-
-module.exports = {
-  webpack(config) {
-    return config;
-  },
-  env: {
-    OP_kEY: process.env.OP_kEY,
-  },
-};
+ 
